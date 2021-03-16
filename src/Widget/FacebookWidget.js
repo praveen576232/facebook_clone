c6a1b8d4414fd9dc5aa48071137194a545dbfae7
praveen576232 <48440981+praveen576232@users.noreply.github.com>
2021-03-16 14:26:54 +0530

    bug fix

--- src/Widget/FacebookWidget.js
+++ src/Widget/FacebookWidget.js
@@ -5,8 +5,7 @@ function FacebookWidget() {
         <div className="facebookwidget">
            
             <iframe 
-            src="https://www.facebook.com/plugins/page.php?
-            href=https%3A%2F%2Fwww.facebook.com%2FCleverProgrammerr%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Freact%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
              width="340" 
              height="100%" 
              style={{border:"none",overflow:"hidden"}}
